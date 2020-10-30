693a88553f7c4339b01e507f92750be54d9cc8d9
Tyler Duncan <Chico55@Tylers-MacBook-Pro.local>
2020-10-30 17:20:28 -0400

    Working on implementing Wall Jumping/Sliding.

--- Boatman/Assets/Scripts/Character2DController.cs
+++ Boatman/Assets/Scripts/Character2DController.cs
@@ -7,45 +7,67 @@ public class Character2DController : MonoBehaviour
     Animator anim;
     public float MovementSpeed = 1;
     public float JumpForce = 1;
-    public bool isRunning = false;
-    private bool turn = false;
-    private bool facingRight = true;
-    private float posX;
-    private float scaleX;
+    public bool isRunning;
+    private bool turn;
+    private bool facingFront = true;
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
 
-    private Rigidbody2D _rigidBody;
+
+    // Wall Jump/Slide Variables
+    public bool isTouchingFront;
+    public Transform frontCheck;
+    public bool wallSliding;
+    public float wallSlideSpeed;
+    public float wallJumpForceX;
+    public float wallJumpForceY;
+
+    private Rigidbody2D rb;
 
     private void Start() {
-        _rigidBody = GetComponent<Rigidbody2D>();
-        posX = transform.position.x;
-        scaleX = transform.localScale.x;
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
     private void Update() {
         
         // Horizontal character movement controller.
-        var movement = Input.GetAxisRaw("Horizontal") * Time.deltaTime * MovementSpeed;
-        transform.position += new Vector3(movement, 0, 0);
+        var movement = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(movement * MovementSpeed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsGround);
 
         // Set animation boolean for running animation. 
         isRunning = (movement != 0); 
         anim.SetBool("isRunning", isRunning);
 
         // Check character movement to get character orientation. 
-        turn = (movement < 0 && facingRight) ^ (movement > 0 && !facingRight);
+        turn = (movement < 0 && facingFront) ^ (movement > 0 && !facingFront);
         if(turn){
-            scaleX = -scaleX;
-            transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
-            facingRight = !facingRight;
+            flip();
         } 
 
         // Character jump controller
-        if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidBody.velocity.y) < 0.001f){
-            _rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
 
-        // Get new character position. 
-        posX = transform.position.x;
+        if (isTouchingFront == true && isGrounded == false && movement != 0){
+            wallSliding = true;
+        } else {
+            wallSliding = false;
+        }
+
+        if(wallSliding){
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+    }
+
+    void flip(){
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        facingFront = !facingFront;
     }
 }
