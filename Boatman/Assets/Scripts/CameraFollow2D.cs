dad1ca8a5734e72d454b408f742cb3def1aefb7e
Tyler Duncan <Chico55@Tylers-MacBook-Pro.local>
2020-11-03 17:03:59 -0500

    Dynamic Jumping Added. Dynamic Camera System Added. Hang Time Implemented. Jump Buffer Implemented. Wall Sliding Implemented. Working on Wall Jumping...

--- Boatman/Assets/Scripts/CameraFollow2D.cs
+++ Boatman/Assets/Scripts/CameraFollow2D.cs
@@ -7,11 +7,6 @@ public class CameraFollow2D : MonoBehaviour
     [SerializeField]
     GameObject player;
 
-    [SerializeField]
-    float timeOffSet;
-
-    [SerializeField]
-    Vector2 posOffSet;
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +17,9 @@ public class CameraFollow2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Cameras current position
-        Vector3 startPos = transform.position;
-
-        // Players current position
-        Vector3 endPos = player.transform.position;
-        endPos.x += posOffSet.x;
-        endPos.y += posOffSet.y;
-        endPos.z += -10;
 
         // This is how you Lerp 
-        transform.position = Vector3.Lerp(startPos, endPos, timeOffSet * Time.deltaTime);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
 
         // This is how you use Smooth Dampening. 
         //transform.position = Vector3.SmoothDamp(startPos, endPos, ref velocity, timeOffSet);
--- Boatman/Assets/Scripts/Character2DController.cs
+++ Boatman/Assets/Scripts/Character2DController.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class Character2DController : MonoBehaviour
 {
-    Animator anim;
-    public float MovementSpeed = 1;
-    public float JumpForce = 1;
-    public bool isRunning;
-    private bool turn;
-    private bool facingFront = true;
-    public bool isGrounded;
+    //  TODO:   
+    //          [] Finish Wall Jumping
+    //          [] Implement Wall sliding on select walls
+    //          [] Implement Particle System for Running
+    //          [] Implement Partical System for Landing
+    //          [] Implement Partical System for Jumping
+
+    private Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer spriteRenderer;
+
+    // Camera Tartget Variables
+    public Transform cameraTarget;
+    public float aheadAmount, aheadSpeed;
+
+    // Player Movement Variables
+    public float movementSpeed = 1, jumpForce = 1, checkRadius, jumpBufferLength;
+    private float jumpBufferCount;
+    public bool isRunning, isGrounded;
     public Transform groundCheck;
-    public float checkRadius;
     public LayerMask whatIsGround;
 
 
     // Wall Jump/Slide Variables
-    public bool isTouchingFront;
     public Transform frontCheck;
-    public bool wallSliding;
-    public float wallSlideSpeed;
-    public float wallJumpForceX;
-    public float wallJumpForceY;
+    public float wallSlideSpeed, wallJumpForceX, wallJumpForceY, wallJumpTime, wallJumpCounter;
+    public bool isTouchingFront, wallJumping, wallSliding;
 
-    private Rigidbody2D rb;
+
+    // Hang time Variables
+    public float hangTime = 0.2f;
+    private float hangCounter;
+
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -32,42 +44,95 @@ public class Character2DController : MonoBehaviour
     }
 
     private void Update() {
-        
-        // Horizontal character movement controller.
-        var movement = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(movement * MovementSpeed, rb.velocity.y);
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-        isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsGround);
+        if (wallJumpCounter <= 0) {
+            // Horizontal character movement controller.
+            var movement = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(movement * movementSpeed, rb.velocity.y);
 
-        // Set animation boolean for running animation. 
-        isRunning = (movement != 0); 
-        anim.SetBool("isRunning", isRunning);
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+            isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsGround);
 
-        // Check character movement to get character orientation. 
-        turn = (movement < 0 && facingFront) ^ (movement > 0 && !facingFront);
-        if(turn){
-            flip();
-        } 
+            // Set animation boolean for running animation. 
+            isRunning = (movement != 0); 
 
-        // Character jump controller
-        if(Input.GetButtonDown("Jump") && isGrounded){
-            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-        }
+            // Check character movement to get character orientation. 
+            if(movement > 0){
+                spriteRenderer.flipX = false;
+            } else if (movement < 0){
+                spriteRenderer.flipX = true;
+            } 
 
-        if (isTouchingFront == true && isGrounded == false && movement != 0){
-            wallSliding = true;
+            // Move Camera Point
+            if(movement != 0){
+                cameraTarget.localPosition = new Vector3(
+                    Mathf.Lerp(cameraTarget.localPosition.x, aheadAmount * movement, aheadSpeed * Time.deltaTime),
+                    cameraTarget.localPosition.y, 
+                    cameraTarget.localPosition.z);
+
+                Debug.Log(cameraTarget.localPosition);
+            }
+
+            // Manage Hang Time
+            if(isGrounded){
+                hangCounter = hangTime;
+            } else {
+                hangCounter -= Time.deltaTime;
+            }
+
+            // Manage Jump Buffer
+            if(Input.GetButtonDown("Jump")){
+                jumpBufferCount = jumpBufferLength;
+            } else {
+                jumpBufferCount -= Time.deltaTime;
+            }
+
+            // Character jump controller
+            if(jumpBufferCount >= 0 && hangCounter > 0){
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                jumpBufferCount = 0;
+            }
+
+            if(Input.GetButtonUp("Jump") && rb.velocity.y > 0){
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
+
+            if (isTouchingFront && !isGrounded && movement != 0){
+                wallSliding = true;
+            } else {
+                wallSliding = false;
+            }
+
+            if(wallSliding){
+                rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+
+                if(Input.GetButtonDown("Jump")){
+                    wallJumpCounter = wallJumpTime;
+                    rb.velocity = new Vector2(-movement * movementSpeed, jumpForce);
+                }
+            }
+
+            if(Input.GetButtonDown("Jump") && wallSliding){
+                wallJumping = true;
+                Invoke("SetWallJumpingToFalse", wallJumpTime);
+            }
+
+            if (wallJumping){
+                rb.velocity = new Vector2(wallJumpForceX * -movement, wallJumpForceY);
+            }
+
         } else {
-            wallSliding = false;
+            wallJumpCounter -= Time.deltaTime;
         }
 
-        if(wallSliding){
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
-        }
+        anim.SetBool("isRunning", isRunning);
     }
 
     void flip(){
-        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        facingFront = !facingFront;
+        //transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    void SetWallJumpingToFalse(){
+        wallJumping = false;
     }
 }
